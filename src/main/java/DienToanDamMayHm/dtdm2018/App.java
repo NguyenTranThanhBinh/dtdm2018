a1184708ab4caf1391655a0cb28884e24f89f5af
NguyenTranThanhBinh <zimminh08@gmail.com>
2018-03-30 17:10:46 +0700

    Initial commit

---
+++ src/main/java/DienToanDamMayHm/dtdm2018/App.java
@@ -0,0 +1,13 @@
+package DienToanDamMayHm.dtdm2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Nguyen Tran Thanh Binh!" );
+    }
+}
